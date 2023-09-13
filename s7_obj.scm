d45dbc3a9c7ba44326dd61276da85422a6aef925
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-09-12 20:18:43 -0400

    Update s7_obj.scm

--- s7_obj.scm
+++ s7_obj.scm
@@ -1,13 +1,17 @@
 (define (make-object . prototypes)
-  (let ((obj (inlet 'events (make-hook 'name 'payload) 'fields (make-hash-table) 'prototypes prototypes)))
-    (openlet obj)
-    obj))
+  (openlet (inlet
+            'events (make-hook 'name 'payload)
+            'fields (make-hash-table)
+            'prototypes prototypes
+            'let-ref-fallback (lambda self&args (apply property self&args))
+            'let-set-fallback (lambda (self name value) (set! (property self name) value)))))
 
 (define (attach-event obj name handler)
   (set! (hook-functions (obj 'events)) (cons
                                         (lambda (h)
                                           (when (eq? (h 'name) name) (handler (h 'payload))))
                                         (hook-functions (obj 'events)))))
+
 (define (call-event obj name payload)
   ((obj 'events) name payload))
 
@@ -18,32 +22,32 @@
            (unless res
              (set! res (call-with-exit (lambda (done)
                                          (for-each (lambda (proto)
-                                                     (let ((val (property proto name)))
+                                                     (let ((val (proto name)))
                                                        (when val (done val)))) (obj 'prototypes))
                                          #f))))
            (unless res (error 'no-such-property obj name))
            (if (null? params)
                res
-               (apply res (append (list obj) params)))))
+               (apply res (cons obj params)))))
    (lambda (obj name newval) (set! ((obj 'fields) name) newval))))
 
 
 (define p (make-object))
 (set! (property p 'foo) 'boogaloo)
-(set! (property p 'classmethod) (lambda (self _) (display (property self 'name))))
+(set! (property p 'classmethod) (lambda (self _) (display self)))
 (define x (make-object p))
 (attach-event x 'foo (lambda (p) (display "foo event on x")))
-(attach-event x 'bar (lambda (p) (display "bar event on x named ") (display (property x 'name))))
-(set! (property x 'name) "bob")
+(attach-event x 'bar (lambda (p) (display "bar event on x named ") (display (x 'name))))
+(set! (x 'name) "bob")
 (call-event x 'foo #f)
 (newline)
 (call-event x 'bar #f)
 (newline)
 (display "object prototype foo property = ")
-(display (property x 'foo))
+(display (x 'foo))
 (newline)
 (display "calling method name = ")
-(property x 'classmethod '_)
+(x 'classmethod 'dummyarg)
 
 
 
